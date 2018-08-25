addf1766dee5ecd50e6553d22bd1a2558db46398
Alex <0alexgutierrez@gmail.com>
2018-08-25 14:13:25 -0700

    started edit item title functionality

--- index.js
+++ index.js
@@ -24,9 +24,9 @@ let filterSearch = '';
 function generateItemElement(item, itemIndex, template) {
   return `
     <li class="js-item-index-element" data-item-index="${itemIndex}">
-      <span class="shopping-item js-shopping-item ${
+      <input type="text" class="shopping-item js-shopping-item ${
   item.checked ? 'shopping-item__checked' : ''
-}">${item.name}</span>
+}" value = "${item.name}">
       <div class="shopping-item-controls">
         <button class="shopping-item-toggle js-item-toggle">
             <span class="button-label">check</span>
